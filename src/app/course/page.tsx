8ee797147cb2c2c8f55c853b40ab01770649023a
Brendan Dash <me@aiwan.run>
2024-05-09 23:33:16 +0800

    feat: done confetti

--- src/app/course/page.tsx
+++ src/app/course/page.tsx
@@ -5,6 +5,7 @@ import type { KeyboardEventHandler } from "react";
 import { useMemo, useState } from "react";
 import { cn } from "twl";
 import { useGlobalData } from "~/atoms/hooks/useGlobalData";
+import Confetti from "~/components/Confetti";
 import { useOnceDoneEffect } from "~/hooks/useOnceDoneEffect";
 import {
   MaterialSymbolsChevronLeft,
@@ -32,6 +33,7 @@ export default function Course() {
   const [currentQuoteIndex, setCurrentQuoteIndex] = useState(0);
   const [isShowTip, setIsShowTip] = useState(false);
   const [inputValue, setInputValue] = useState("");
+  const [passed, setPassed] = useState(false);
 
   const currentCourse = useMemo(() => {
     return globalData.find((item) => item.title.en === name);
@@ -67,10 +69,13 @@ export default function Course() {
 
     if (isCorrect) {
       doneQuoteTask(name!, currentQuoteIndex);
-
-      setCurrentQuoteIndex((prev) => prev + 1);
-      setInputValue("");
-      setIsShowTip(false);
+      if (currentCourse?.quotes.length === currentQuoteIndex + 1) {
+        setPassed(true);
+      } else {
+        setCurrentQuoteIndex((prev) => prev + 1);
+        setInputValue("");
+        setIsShowTip(false);
+      }
     }
   };
 
@@ -116,77 +121,80 @@ export default function Course() {
   const { title } = currentCourse;
 
   return (
-    <div className="flex h-full flex-col gap-y-2">
-      <nav className="flex justify-end px-4 py-2">
-        <Link href="/">
-          <MaterialSymbolsHome className="text-pink-400" />
-        </Link>
-      </nav>
-      <header className="mt-12 text-center text-pink-400">
-        <div>{title.zh}</div>
-        <div>{title.en}</div>
-        <div>
-          {currentQuoteIndex + 1}/{currentCourse.quotes.length}
-        </div>
-      </header>
-      <main className="mx-20 flex flex-1 items-center justify-center text-center">
-        <div className="flex flex-col gap-y-12">
+    <>
+      <Confetti passed={passed} />
+      <div className="flex h-full flex-col gap-y-2">
+        <nav className="flex justify-end px-4 py-2">
+          <Link href="/">
+            <MaterialSymbolsHome className="text-pink-400" />
+          </Link>
+        </nav>
+        <header className="mt-12 text-center text-pink-400">
+          <div>{title.zh}</div>
+          <div>{title.en}</div>
           <div>
-            <div className="flex items-center justify-center gap-x-2 text-2xl font-medium text-pink-800">
-              <span>{currentQuote.zh}</span>
-              {isShowTip ? (
-                <PhEyeBold
-                  className="cursor-pointer"
-                  onClick={() => setIsShowTip(false)}
-                />
-              ) : (
-                <PhEyeClosed
-                  className="cursor-pointer"
-                  onClick={() => setIsShowTip(true)}
-                />
-              )}
-            </div>
-            <div className="flex items-center justify-center gap-x-2 px-4 text-3xl font-medium text-pink-800">
-              <span
-                className={cn(
-                  "inline-flex items-center gap-x-1",
-                  isShowTip ? "opacity-100" : "opacity-0",
+            {currentQuoteIndex + 1}/{currentCourse.quotes.length}
+          </div>
+        </header>
+        <main className="mx-20 flex flex-1 items-center justify-center text-center">
+          <div className="flex flex-col gap-y-12">
+            <div>
+              <div className="flex items-center justify-center gap-x-2 text-2xl font-medium text-pink-800">
+                <span>{currentQuote.zh}</span>
+                {isShowTip ? (
+                  <PhEyeBold
+                    className="cursor-pointer"
+                    onClick={() => setIsShowTip(false)}
+                  />
+                ) : (
+                  <PhEyeClosed
+                    className="cursor-pointer"
+                    onClick={() => setIsShowTip(true)}
+                  />
                 )}
-              >
-                {currentQuote.en.split(" ").map((word, index) => (
-                  <span
-                    key={index}
-                    className={cn(
-                      isEqualWord(word, inputValueArr[index])
-                        ? "text-pink-400"
-                        : "text-gray-400",
-                    )}
-                  >
-                    {word}
-                  </span>
-                ))}
-              </span>
+              </div>
+              <div className="flex items-center justify-center gap-x-2 px-4 text-3xl font-medium text-pink-800">
+                <span
+                  className={cn(
+                    "inline-flex items-center gap-x-1",
+                    isShowTip ? "opacity-100" : "opacity-0",
+                  )}
+                >
+                  {currentQuote.en.split(" ").map((word, index) => (
+                    <span
+                      key={index}
+                      className={cn(
+                        isEqualWord(word, inputValueArr[index])
+                          ? "text-pink-400"
+                          : "text-gray-400",
+                      )}
+                    >
+                      {word}
+                    </span>
+                  ))}
+                </span>
+              </div>
+            </div>
+            <input
+              type="text"
+              className="border-b-2 text-center text-3xl font-medium outline-none"
+              value={inputValue}
+              onInput={(e) => setInputValue(e.currentTarget.value)}
+              onKeyDown={handleInputKeyDown}
+            />
+            <div className="flex items-center justify-center gap-x-12">
+              <MaterialSymbolsChevronLeft
+                onClick={() => handleMoveLeft()}
+                className="size-8 cursor-pointer"
+              />
+              <MaterialSymbolsChevronRightRounded
+                onClick={() => handleMoveRight()}
+                className="size-8 cursor-pointer"
+              />
             </div>
           </div>
-          <input
-            type="text"
-            className="border-b-2 text-center text-3xl font-medium outline-none"
-            value={inputValue}
-            onInput={(e) => setInputValue(e.currentTarget.value)}
-            onKeyDown={handleInputKeyDown}
-          />
-          <div className="flex items-center justify-center gap-x-12">
-            <MaterialSymbolsChevronLeft
-              onClick={() => handleMoveLeft()}
-              className="size-8 cursor-pointer"
-            />
-            <MaterialSymbolsChevronRightRounded
-              onClick={() => handleMoveRight()}
-              className="size-8 cursor-pointer"
-            />
-          </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+    </>
   );
 }
