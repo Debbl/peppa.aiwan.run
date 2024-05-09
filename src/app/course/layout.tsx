972dc2ff6847e9728ce5daaf22502acd76200db4
Brendan Dash <me@aiwan.run>
2024-05-09 19:22:17 +0800

    fix: useSearchParams Suspense

---
+++ src/app/course/layout.tsx
@@ -0,0 +1,9 @@
+import { Suspense } from "react";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <Suspense fallback={<div>loading</div>}>
+      <>{children}</>
+    </Suspense>
+  );
+}
