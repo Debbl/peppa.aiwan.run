180fd75e80d8452065d7b82e18abf0ecfef2d9ef
Brendan Dash <me@aiwan.run>
2024-05-18 09:13:46 +0800

    feat: use react compiler

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,20 @@
 // @ts-check
 import { config } from "@debbl/eslint-config";
+import pluginReactCompiler from "eslint-plugin-react-compiler";
 
 export default config({
   typescript: true,
   tailwindcss: true,
   react: {
     next: true,
+  },
+  customConfig: {
+    files: ["src/**/*.{jsx,tsx}"],
+    plugins: {
+      "react-compiler": pluginReactCompiler,
+    },
+    rules: {
+      "react-compiler/react-compiler": "error",
+    },
   },
 });
